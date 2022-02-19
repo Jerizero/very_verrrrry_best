bd371c7a8dff319786f086cadcf6251f681cd013
anonymous anonymous <jerielacos@gmail.com>
2022-02-19 21:54:18 +0000

    Direct association between users and verybests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :verybests,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/verybest.rb
+++ app/models/verybest.rb
@@ -1,6 +1,8 @@
 class Verybest < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   belongs_to :venue
 
   belongs_to :dish
